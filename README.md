23048d9c219d95b62027bb3aab54499419bbef8d
Dan Visel <dbvisel@gmail.com>
2020-09-09 10:16:12 +0800

    added Google Analytics

--- README.md
+++ README.md
@@ -4,36 +4,39 @@ Gatsby-based version of site reporter code
 
 Author on report config should be default, overridden by text's author if it exists
 
+BUGS:
+
+- http://localhost:8000/scc/dataset/eware/id/eware_006XX/
+
+Image link is poorly formed. Fix this.
+
 TODO:
- * datasets:
-   * Maybe move to CSS grid?
-   * thumbnails + image records
-	 * links to object pages (more of them)
- * object pages
-	 * multiple images?
-	 * multiple linked records: http://localhost:8000/scc/dataset/eware/id/eware_3902
- * navigation:
-	 * 404 menu? Should this be smart or not?
- * text:
-	 * React doesn't like the weird namespace thing in the footer
- * build
-   * better way to deal with images?
-	 * is there another path that we're using for object?
-	 * do I actually need to generate so many object pages?
- * data
-	 * jambi? probably doesn't exist
+
+- datasets:
+  - Maybe move to CSS grid?
+  - thumbnails + image records \* links to object pages (more of them)
+- object pages
+  _ multiple images?
+  _ multiple linked records: http://localhost:8000/scc/dataset/eware/id/eware_3902
+- navigation: \* 404 menu? Should this be smart or not?
+- text: \* React doesn't like the weird namespace thing in the footer
+- build
+  - better way to deal with images?
+    _ is there another path that we're using for object?
+    _ do I actually need to generate so many object pages?
+- data \* jambi? probably doesn't exist
 
 Could use ThemeProvider for a light/dark theme switcher: https://css-tricks.com/a-dark-mode-toggle-with-react-and-themeprovider/
 
 # Table header remember:
 
- * https://css-tricks.com/position-sticky-and-table-headers/
- * https://github.com/thebuilder/react-intersection-observer#readme
- 
+- https://css-tricks.com/position-sticky-and-table-headers/
+- https://github.com/thebuilder/react-intersection-observer#readme
+
 Maybe use <datalist> rather than <select> in table headers? Does that make sense? We could use that for filtering.
 
 ## REMEMBER
 
- * This is using Git/Netlify LFS, as described here: https://css-tricks.com/getting-netlify-large-media-going/
-   If there's trouble, try source /Users/dan/.netlify/helper/path.bash.inc
-	 (does this break everything?)
+- This is using Git/Netlify LFS, as described here: https://css-tricks.com/getting-netlify-large-media-going/
+  If there's trouble, try source /Users/dan/.netlify/helper/path.bash.inc
+  (does this break everything?)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,71 @@
 module.exports = {
   pathPrefix: `/sitereports`,
-	siteMetadata: {
-		title: `sr3`,
-		description: `NUS Press Site Reports`,
-		author: `Dan Visel <dbvisel@gmail.com>`
-	},
-	plugins: [
-		`gatsby-plugin-react-helmet`,
-		{
-			resolve: 'gatsby-mdx',
-			options: {
-				extensions: ['.mdx'],
-				gatsbyRemarkPlugins: [
-					// more on this later
-				]
-			}
-		},
-		{
-			resolve: `gatsby-transformer-json`,
-			options: {
-				typeName: `allTheJson` // a fixed string
-			}
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `images`,
-				path: `${__dirname}/src/images`
-			}
-		},
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				path: `${__dirname}/data`,
-				name: 'markdown-pages'
-			}
-		},
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-sharp`,
-		{
-			resolve: `gatsby-plugin-styled-components`,
-			options: {}
-		},
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `NUS Site Reports`,
-				short_name: `sr3`,
-				start_url: `/sitereports/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
-				display: `minimal-ui`,
-				icon: `src/images/nus-icon.png` // This path is relative to the root of the site.
-			}
-		},
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		//`gatsby-plugin-offline`
-	]
+  siteMetadata: {
+    title: `sr3`,
+    description: `NUS Press Site Reports`,
+    author: `Dan Visel <dbvisel@gmail.com>`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-35006494-3",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+      },
+    },
+    {
+      resolve: "gatsby-mdx",
+      options: {
+        extensions: [".mdx"],
+        gatsbyRemarkPlugins: [
+          // more on this later
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-transformer-json`,
+      options: {
+        typeName: `allTheJson`, // a fixed string
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/data`,
+        name: "markdown-pages",
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `NUS Site Reports`,
+        short_name: `sr3`,
+        start_url: `/sitereports/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/nus-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    //`gatsby-plugin-offline`
+  ],
 };
