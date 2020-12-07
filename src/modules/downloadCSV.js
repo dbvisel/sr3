94f17dbcfd5e5796ced86f7e6cdaf8e6a5100285
Dan Visel <dbvisel@gmail.com>
2020-12-07 12:25:13 +0800

    version with downloading csv ability

---
+++ src/modules/downloadCSV.js
@@ -0,0 +1,70 @@
+const convertToCSV = (objArray) => {
+  var array = typeof objArray !== "object" ? JSON.parse(objArray) : objArray;
+  var str = "";
+
+  for (var i = 0; i < array.length; i++) {
+    var line = "";
+    for (var index in array[i]) {
+      if (line !== "") line += ",";
+
+      line += array[i][index];
+    }
+
+    str += line + "\r\n";
+  }
+
+  return str;
+};
+
+function exportCSVFile(headers, items, fileTitle) {
+  if (headers) {
+    items.unshift(headers);
+  }
+
+  // Convert Object to JSON
+  var jsonObject = JSON.stringify(items);
+  var csv = convertToCSV(jsonObject);
+  var exportedFilenmae = fileTitle + ".csv" || "export.csv";
+  var blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+  if (navigator.msSaveBlob) {
+    // IE 10+
+    navigator.msSaveBlob(blob, exportedFilenmae);
+  } else {
+    var link = document.createElement("a");
+    if (link.download !== undefined) {
+      // feature detection
+      // Browsers that support HTML5 download attribute
+      var url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", exportedFilenmae);
+      link.style.visibility = "hidden";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+}
+
+const downloadCSV = (headersIn, datasetIn, reportTitle) => {
+  const headers = {};
+  for (let i = 0; i < headersIn.length; i++) {
+    if (headersIn[i].fieldName) {
+      headers[headersIn[i].fieldKey] = headersIn[i].fieldName.replace(/,/g, "");
+    }
+  }
+
+  const dataset = [];
+  for (let i = 0; i < datasetIn.length; i++) {
+    const cleanedRecord = {};
+    for (const [key] of Object.entries(headers)) {
+      cleanedRecord[key] = datasetIn[i][key]
+        ? datasetIn[i][key].replace(/,/g, "")
+        : "";
+    }
+    dataset.push(cleanedRecord);
+  }
+
+  exportCSVFile(headers, dataset, reportTitle);
+};
+
+export default downloadCSV;
--- src/templates/datasetpage.js
+++ src/templates/datasetpage.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import Layout from './../components/layout';
-import Dataset from './../components/dataset';
+import React from "react";
+import Layout from "./../components/layout";
+import Dataset from "./../components/dataset";
+import downloadCSV from "./../modules/downloadCSV";
 
 // TODO: do we set dataset anything else?
 
 export default function DataSetPage({ pageContext }) {
-	const { reportData, data } = pageContext;
-	let thisPath = `/${reportData.id}/dataset/${data.id}`;
-	return (
-		<Layout title={data.name} menu={reportData} thisPage={thisPath}>
-			<article>
-				<Dataset data={data} />
-			</article>
-		</Layout>
-	);
+  const { reportData, data } = pageContext;
+
+  let thisPath = `/${reportData.id}/dataset/${data.id}`;
+  return (
+    <Layout title={data.name} menu={reportData} thisPage={thisPath}>
+      <article>
+        <Dataset data={data} />
+        <p>
+          <a
+            style={{ textDecoration: "none", color: "var(--orange)" }}
+            href={`/#`}
+            onClick={(e) => {
+              e.preventDefault();
+              downloadCSV(
+                data.fields,
+                data.data,
+                `${data.reportID}-${data.id}`
+              );
+            }}
+          >
+            <strong>Download CSV</strong>
+          </a>
+        </p>
+      </article>
+    </Layout>
+  );
 }
