39c68330e9ed0f733c672ec75a95f1c9e679f7ab
Dan Visel <dbvisel@gmail.com>
2022-02-08 15:53:11 +0800

    made it look more like peter's

--- src/components/CardGrid/elements.js
+++ src/components/CardGrid/elements.js
@@ -1,70 +1,113 @@
 import styled from "styled-components";
-
-const colors = [
-  "#e8e800",
-  "#7ffd00",
-  "#3dff9e",
-  "#00fafa",
-  "#c9e4ff",
-  "#dedeff",
-  "#ecdaff",
-  "#ffd3ff",
-  "#ffd5ea",
-  "#ffd7d7",
-  "#ffdab6",
-  "#ffdab6",
-];
-
-const getColor = (index) => colors[index % colors.length];
+import { withPrefix } from "gatsby";
 
 export const CardGridWrapper = styled.div`
   --insidePadding: 12px;
   --gutter: 16px;
   --cardWidth: 180px;
-  --cardHeight: 180px;
+  --cardHeight: 360px;
   --borderRadius: 4px;
-  margin-top: var(--gutter);
-  margin-right: calc(0px - var(--gutter));
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: start;
-  align-items: flex-start;
+  max-width: 100%;
+  overflow-x: hidden;
+  & > div {
+    padding: var(--gutter);
+    padding-top: 25px;
+    display: flex;
+    justify-content: start;
+    align-items: flex-start;
+    min-width: 100%;
+    overflow-x: scroll;
+  }
 `;
 
 export const CardGridCard = styled.div`
-  border: 1px solid var(--black);
   margin: 0 var(--gutter) var(--gutter) 0;
-  width: var(--cardWidth);
+  min-width: var(--cardWidth);
   min-height: var(--cardHeight);
   border-radius: var(--borderRadius);
   transition: 0.5s;
-  background-color: ${(props) => getColor(props.color)};
   user-select: none;
+  flex: 1;
+  background-image: url(${(props) => withPrefix(props.image)});
+  background-size: cover;
+  background-position: center center;
+  position: relative;
+  background-color: ${(props) => props.color};
+  box-shadow: 0 0 var(--gutter) rgba(0, 0, 0, 0.125);
+  &:before {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: ${(props) => props.color};
+    opacity: 0.5;
+    z-index: 1;
+  }
   & > a {
+    position: absolute;
+    z-index: 2;
     display: flex;
     flex-direction: column;
     width: 100%;
-    height: 100%;
+    min-height: var(--cardHeight);
     padding: var(--insidePadding);
     border-bottom: none !important;
+    color: var(--white) !important;
+    justify-content: center;
+    transition: 0.5s;
     & h3 {
-      margin: 0 0 var(--insidePadding);
-      font-size: 16px;
+			font-family: var(--textFont)
+      margin: 0;
+      text-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+      font-size: 20px;
     }
     & h4 {
+			font-family: var(--textFont)
       margin: 0;
       font-weight: normal;
-      font-size: 14px;
+      font-size: 16px;
       margin: 0 0 var(--insidePadding);
+      text-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
     }
     & p {
-      text-align: right;
+      width: 100%;
+      text-align: left;
       font-style: italic;
       font-family: var(--headerFont);
-      font-size: 11px;
+      text-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
     }
   }
   &:hover {
     transform: scale(1.05);
+  }
+`;
+
+export const LocationList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  & li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: inline-flex;
+    align-items: center;
+    & span {
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      border-radius: 100%;
+      margin-right: 4px;
+    }
+    & + li {
+      margin-left: 16px;
+    }
   }
 `;
--- src/components/CardGrid/index.js
+++ src/components/CardGrid/index.js
@@ -1,6 +1,15 @@
 import * as React from "react";
 import { useStaticQuery, graphql, Link } from "gatsby";
-import { CardGridWrapper, CardGridCard } from "./elements";
+import { CardGridWrapper, CardGridCard, LocationList } from "./elements";
+
+const colors = {
+  Singapore: "#e8e800",
+  Indonesia: "#00fafa",
+  Myanmar: "#ffd3ff",
+  other: "#ffdab6",
+};
+
+const getColor = (index) => (colors[index] ? colors[index] : colors["other"]);
 
 const hideExcludedReports = false;
 
@@ -22,8 +31,10 @@ const CardGrid = () => {
             id
             doi
             date
+            location
             lastUpdated
             title
+            image
           }
         }
       }
@@ -54,19 +65,35 @@ const CardGrid = () => {
     return 0;
   });
 
+  const locations = new Set(sortedReports.map((report) => report.location));
+
   return (
     <section>
-      <h2>Reports:</h2>
-      <CardGridWrapper>
-        {sortedReports.map((report, index) => (
-          <CardGridCard key={index} color={index}>
-            <Link to={`/${report.id}`}>
-              <h3>{report.title}</h3>
-              <h4>{report.author}</h4>
-              <p>Updated: {report.lastUpdated}</p>
-            </Link>
-          </CardGridCard>
+      <h2 style={{ textAlign: "center", margin: "25px 0" }}>Reports:</h2>
+      <LocationList>
+        {[...locations].map((location, index) => (
+          <li key={index}>
+            <span style={{ backgroundColor: getColor(location) }} />
+            {location}
+          </li>
         ))}
+      </LocationList>
+      <CardGridWrapper>
+        <div>
+          {sortedReports.map((report, index) => (
+            <CardGridCard
+              key={index}
+              color={getColor(report.location)}
+              image={`/${report.id}/images/${report.image}`}
+            >
+              <Link to={`/${report.id}`}>
+                <h3>{report.title}</h3>
+                <h4>{report.date || report.lastUpdated}</h4>
+                <p>&gt;&gt; Report</p>
+              </Link>
+            </CardGridCard>
+          ))}
+        </div>
       </CardGridWrapper>
     </section>
   );
